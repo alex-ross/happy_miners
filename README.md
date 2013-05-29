149bf0451e5d629d1fe417944f964bfdbd301420
Alexander Ross <alexander.ross@standout.se>
2013-05-29 22:06:26 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,18 @@
+# Happy miners
+
+## Ruby version
+`jruby 1.7.4`
+
+## System dependencies
+
+## Configuration
+
+## Database creation
+
+## Database initialization
+
+## How to run the test suite
+
+## Services (job queues, cache servers, search engines, etc.)
+
+## Deployment instructions
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HappyMiners::Application.config.secret_key_base = '3647b5a459a9f7c544b84324dae3d6cbec1bc050bcc22e77f8114150c0e85e79ac9a6089a9c71e96ef9367049144de2817f4427a702685968adcaeb797d7f8f7'
